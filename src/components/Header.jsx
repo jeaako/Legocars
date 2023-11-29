6810840408272bc4fffde2902d8c6190c4c754d7
JoaquinCarrillo17 <joaquin.carrilloy@um.es>
2023-11-29 13:50:41 +0100

    Mejora del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,47 @@
 // src/components/Header.jsx
-//import React from 'react';
+import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Link } from '@mui/material';
+import LoginIcon from '../../resources/acceso.png';
+import './Header.css';
+
 
 const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
+    <AppBar position="static" sx={{ width: '100%' }}>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
         <Typography variant="h6">Legocars</Typography>
-        <Button color="inherit">
-          <Link href="#" color="inherit">
-            Home
-          </Link>
-        </Button>
-        <Button color="inherit">
-          <Link href="#" color="inherit">
-            Campeonato
-          </Link>
-        </Button>
-        <Button color="inherit">
-          <Link href="#" color="inherit">
-            Otro enlace
-          </Link>
-        </Button>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Button color="inherit">
+            <Link href="#" color="inherit">
+              Home
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="#" color="inherit">
+              Campeonato
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="#" color="inherit">
+              Otro enlace
+            </Link>
+          </Button>
+          <img
+            src={LoginIcon}
+            alt="Iniciar Sesión"
+            style={{
+              width: '30px',
+              height: '30px',
+              cursor: 'pointer',
+              marginLeft: '10px', 
+            }}
+          />
+        </div>
       </Toolbar>
     </AppBar>
   );
 };
+
+
 
 export default Header;
